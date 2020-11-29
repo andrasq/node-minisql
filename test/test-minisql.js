83621d73501b860c363950ae3c5f3947b42bfcdf
Andras <andras@andrasq.com>
2020-11-28 22:47:11 -0500

    more tests

--- test/test-minisql.js
+++ test/test-minisql.js
@@ -26,8 +26,69 @@ describe('minisql', function() {
         packeteer = packman.packeteer
         socket = new events.EventEmitter()
         socket.setNoDelay = noop
-        qmock.stubOnce(net, 'connect').returns(socket)
+        qmock.stubOnce(net, 'connect', function() {
+            setImmediate(function() { socket.emit('connect') })
+            return socket
+        })
         done()
+    })
+
+    describe('Packeteer', function() {
+        it('tallies total length', function(done) {
+            packeteer.write(fromBuf("abc"))
+            assert.equal(packeteer.nbytes, 3)
+            packeteer.write(fromBuf("defghi"))
+            assert.equal(packeteer.nbytes, 9)
+            done()
+        })
+        it('grows first packet to 5', function(done) {
+            packeteer.write(fromBuf([1, 0, 0]))
+            packeteer.write(fromBuf([4]))
+            packeteer.write(fromBuf([5, 6]))
+            assert.equal(packeteer.bufs.length, 1)
+            packeteer.write(fromBuf([7, 8]))
+            assert.equal(packeteer.bufs.length, 2)
+            assert.deepEqual(packeteer.bufs[0], [1, 0, 0, 4, 5, 6])
+            assert.deepEqual(packeteer.bufs[1], [7, 8])
+            done()
+        })
+    })
+
+    describe('Packman', function() {
+        beforeEach(function(done) {
+            packman.connect({}, function() {
+                done()
+            })
+        })
+        it('gathers first socket error', function(done) {
+            assert.equal(packman.error, null)
+            socket.emit('error', 'mock-error-1')
+            assert.equal(packman.error, 'mock-error-1')
+            socket.emit('error', 'mock-error-2')
+            assert.equal(packman.error, 'mock-error-1')
+            done()
+        })
+        it('end calls socket.end', function(done) {
+            var spy = qmock.stub(socket, 'end', function() { socket.emit('close') })
+            packman.end()
+            assert.ok(spy.called)
+            done()
+        })
+        it('end calls callback', function(done) {
+            var spy = qmock.stub(socket, 'end', function() { socket.emit('close') })
+            packman.end(function(err) {
+                done()
+            })
+        })
+        it('end just returns existing error', function(done) {
+            var spy = qmock.stub(socket, 'end')
+            packman.error = 'mock error'
+            packman.end(function(err) {
+                assert.equal(err, 'mock error')
+                assert.ok(!spy.called)
+                done()
+            })
+        })
     })
 
     describe('_select', function() {
