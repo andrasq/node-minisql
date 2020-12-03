b1f1911534ede54a7eb1509c7ed0de7f4c02a43c
Andras <andras@andrasq.com>
2020-12-02 22:20:00 -0500

    utils fixes, better test coverage, test decodeBytesTo; v0.7.0

--- test/test-utils.js
+++ test/test-utils.js
@@ -2,6 +2,7 @@
 
 var assert = require('assert')
 var utils = require('../lib/utils')
+var qmock = require('qmock')
 
 describe('utils', function() {
     describe('Bytes', function() {
@@ -61,6 +62,58 @@ describe('utils', function() {
             assert.strictEqual(bytes.getStringV(), null)
             bytes.seek(1)
             assert.strictEqual(bytes.getBinaryV(), null)
+            done()
+        })
+        it('decodes bytes according to the format', function(done) {
+            assert.deepEqual(bytes.open([1, 2, 3, 4, 5, 6]).decodeBytesTo({}, '123', ['a', 'b', 'c']),
+                { a: 1, b: 2 + 3*256, c: 4 + 5*256 + 6*256*256 })
+            assert.deepEqual(bytes.open([1, 2, 3, 4, 5, 6]).decodeBytesTo({}, '321', ['a', 'b', 'c']),
+                { a: 1 + 2*256 + 3*256*256, b: 4 + 5*256, c: 6 })
+            assert.deepEqual(bytes.open([1, 2, 3, 4, 5, 6]).decodeBytesTo({}, '4', ['a', 'b', 'c']),
+                { a: 1 + 2*256 + 3*256*256 + 4*256*256*256 })
+            assert.deepEqual(bytes.open([1, 2, 3, 4, 5, 6]).decodeBytesTo({}, 'vvv', ['a', 'b', 'c']),
+                { a: 1, b: 2, c: 3 })
+            assert.deepEqual(bytes.open([252, 2, 3, 4, 5, 6]).decodeBytesTo({}, 'vv2', ['a', 'b', 'c']),
+                { a: 2 + 3*256, b: 4, c: 5 + 6*256 })
+            assert.deepEqual(bytes.open([253, 2, 3, 4, 5, 6]).decodeBytesTo({}, 'vv', ['a', 'b', 'c']),
+                { a: 2 + 3*256 + 4*256*256, b: 5 })
+            assert.deepEqual(bytes.open([251, 2, 3, 4, 5, 6]).decodeBytesTo({}, 'vv', ['a', 'b', 'c']),
+                { a: null, b: 2 })
+            assert.deepEqual(bytes.open([251, 0x42, 0x43, 0x44, 0, 0]).decodeBytesTo({}, 'S', ['a']),
+                { a: null })
+            assert.deepEqual(bytes.open([2, 0x42, 0x43, 0x44, 0, 0]).decodeBytesTo({}, 'S', ['a']),
+                { a: 'BC' })
+            assert.deepEqual(bytes.open([0x41, 0x42, 0x43, 0x44, 0, 0]).decodeBytesTo({}, 'e', ['a']),
+                { a: 'ABCD\x00\x00' })
+            done()
+        })
+        it('decodes z format with getStringZ', function(done) {
+            var spy = qmock.spy(bytes, 'getStringZ')
+            bytes.open([0x41, 0x42, 0x43, 0x44, 0]).decodeBytesTo({}, 'z', ['a'])
+            assert.ok(spy.called)
+            done()
+        })
+        it('decodes SNB formats with V getters', function(done) {
+            var spyS = qmock.spy(bytes, 'getStringV')
+            var spyN = qmock.spy(bytes, 'getNumberV')
+            var spyB = qmock.spy(bytes, 'getBinaryV')
+            bytes.open([0xfb, 0xfb, 0xfb]).decodeBytesTo({}, 'SNB', ['a', 'b', 'c'])
+            assert.ok(spyS.called)
+            assert.ok(spyN.called)
+            assert.ok(spyN.calledAfter(spyS))
+            assert.ok(spyB.called)
+            assert.ok(spyB.calledAfter(spyN))
+            done()
+        })
+    })
+
+    describe('pairTo', function() {
+        it('builds hashes from names,values arrays', function(done) {
+            assert.deepEqual(utils.pairTo({}, [], []), {})
+            assert.deepEqual(utils.pairTo({}, ['a', 'b'], [1, 2.5]), { a: 1, b: 2.5 })
+            assert.deepEqual(utils.pairTo({}, ['a', 'b'], [1, 2, 3]), { a: 1, b: 2 })
+            assert.deepEqual(utils.pairTo({}, ['a', 'b'], [1]), { a: 1, b: undefined })
+            assert.deepEqual(utils.pairTo({ x: 1 }, ['a', 'b'], ['two', 3]), { x: 1, a: 'two', b: 3 })
             done()
         })
     })
